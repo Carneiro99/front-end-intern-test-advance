9793b6f187d45153ea79ac0e9b88c29e564e7581
Rafael Carneiro <r.carneirodepaula@gmail.com>
2019-08-26 19:36:43 -0300

    Solução dos campos orbrigatórios, linkando a classe highlightsForm do .CSS

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
-// Adicionamos nosso código dentro de uma função anônima para evitar
-// variáveis globais.
+
 (function() {
-  // Elemento principal do formulário
+  
   const contactForm = document.getElementById('contact');
 
-  // Campos do formulário
+  
   const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
   const messageInput = document.getElementById('message');
-
-  // "addEventListener" instrui o navegador a executar uma função sempre
-  // que um evento específico acontece. Nesse caso, a função abaixo será
-  // executada toda vez que o botão de envio for pressionado.
-  contactForm.addEventListener('submit', function() {
-    // A propriedade "value" contém o valor atual de um input ou textarea
+  
+  
+  
+  
+  contactForm.addEventListener('submit',function () {
+  
+   
     const nameValue = nameInput.value;
 
-    // A propriedade "length" contém o tamanho (em caracteres) de uma
-    // uma string. Toda string contém essa propriedade.
+    
     const nameLength = nameValue.length;
+    const emailValue = emailInput.value;
+    const messageValue = message.value;
 
-    // "window" é um objeto global que representa a janela (ou aba) do
-    // navegador que está executando o código do seu site. O método
-    // "alert" simplesmente mostra um aviso para o usuário contendo a
-    // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
 
-    // Altere e complete essa função para validar os campos do formulário
-    // de acordo com as especificações do teste. Boa sorte!
+    
+    
+      if(nameValue == ""){
+          event.preventDefault();
+          window.alert('Preencha os campos obrigatórios!')
+          document.getElementById('name').className = 'highlightsForm'
+          return false;
+          
+      } 
+      
+      else if(emailValue == ""){
+          event.preventDefault();
+          window.alert('Preencha os campos obrigatórios!')
+          document.getElementById('email').className = 'highlightsForm'
+          return false
+          
+      } 
+      
+      else if(messageValue == ""){
+          event.preventDefault();
+          window.alert('Preencha os campos obrigatórios!')
+          document.getElementById('message').className = 'highlightsTextForm'
+          return false;
+
+
+      }  else  {   window.alert(`Obrigado pelo contato, ${nameValue}!`);  
+       }
+    
   });
+
 })();
